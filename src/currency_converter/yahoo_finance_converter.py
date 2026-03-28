e203689dc667fb482f3ffd5173004d1ab5f9a8a9
Alex Stuart McNall <asm@alexmcnall.co.uk>
2026-03-28 16:53:41 +0000

    Add Yahoo Finance currency converter service class

---
+++ src/currency_converter/yahoo_finance_converter.py
@@ -0,0 +1,103 @@
+import datetime
+
+import yfinance
+
+from currency_converter_service import CurrencyConverterService
+
+class YahooFinanceConverter(CurrencyConverterService):
+    '''
+    Use the yfinance module to get currency conversion rates using Yahoo Finance's forex data.
+    '''
+    @staticmethod
+    def _identify_ticker(base: str, quote: str) -> str:
+        '''
+        Attempt to identify the Yahoo Finance forex symbol for the given
+        currencies.
+
+        Parameters
+        ----------
+        base : str
+            The currency that the value is being converted from.
+        quote: str
+            The currency that the value is being converted to.
+
+        Returns
+        -------
+        str
+            The ticker for the identified forex pair.
+        '''
+        MAX_COUNT = 50
+
+        # Check that the symbols are not the exact same
+        if base == quote:
+            raise ValueError(f"Base and quote currencies are identical: {base}")
+
+        # Make a guess for the currency pair name (e.g GBP/USD)
+        guess = base + "/" + quote
+        
+        # Perform lookup for currency pair
+        search = yfinance.Lookup(guess).get_currency(count=MAX_COUNT)
+
+        # Check if search not empty
+        if not search.empty:
+            # If an exact match is found, return its symbol
+            if search["shortName"].iloc[0] == guess:
+                return search["shortName"].index[0]
+
+        # If it isn't found, give up and throw an exception
+        raise Exception(f"Yahoo Finance could not find a currency pair for the base and quote currencies {base} and {quote} respectively")
+    
+    def get_exchange_rate(
+        self,
+        base,
+        quote,
+        date: datetime.datetime | datetime.date | None = None,
+    ) -> float:
+        '''
+        Get the exchange rate between the base and quote currencies
+
+        Recover exchange rates from Yahoo Finance forex tickers. This method
+        calls another method to find the correct ticker before getting the
+        forex price history. A simple check to see if recent price information
+        exists is performed. If no recent price information can be found, an
+        exception is thrown.
+
+        Parameters
+        ----------
+        base : str
+            The currency that the value is being converted from.
+        quote: str
+            The currency that the value is being converted to.
+        date: {date, datetime, None}, optional
+            The time to use for the exchange rate. If None, then default to
+            the most recent exchange rate.
+
+        Returns
+        -------
+        float
+            Forex rate at the close of the price interval (minute, day, etc.)
+        '''
+
+        # Recover the correct forex pair
+        ticker = YahooFinanceConverter._identify_ticker(base, quote)
+
+        # Truncate datetime to day (yfinance only provides daily values)
+        if isinstance(date, datetime.datetime):
+            date = date.replace(hour=0, minute=0, second=0, microsecond=0)
+
+        # Create ticker object
+        yf_ticker = yfinance.Ticker(ticker)
+
+        # Attempt to recover price
+        price_data = yf_ticker.history(start=date, period="1d")
+        
+        # Check if a value was returned
+        if price_data.empty:
+            # Check for prices for the previous two weeks
+            price_data = yf_ticker.history(end=date, period="14d")
+
+            if price_data.empty:
+                raise Exception(f"No price information is available for the last two weeks for {ticker}.")
+
+        return price_data['Close'].iloc[-1]
+
---
+++ tests/test_yahoo_finance_converter.py
@@ -0,0 +1,91 @@
+import datetime, numpy
+
+import pytest, yfinance
+
+from yahoo_finance_converter import YahooFinanceConverter
+
+def test_symbol_lookup_gbpusd():
+    '''Test the symbol lookup for GBP to USD works'''
+    converter = YahooFinanceConverter()
+    symbol = converter._identify_ticker("GBP", "USD")
+    assert symbol == "GBPUSD=X"
+
+def test_symbol_lookup_usdgbp():
+    '''Test the symbol lookup for USD to GBP works'''
+    converter = YahooFinanceConverter()
+    symbol = converter._identify_ticker("USD", "GBP")
+    assert symbol == "GBP=X"
+
+def test_symbol_lookup_usdusd():
+    '''Test for an error when trying to convert to the original currency'''
+    converter = YahooFinanceConverter()
+    with pytest.raises(ValueError):
+        symbol = converter._identify_ticker("USD", "USD")
+
+def test_exchange_rate_usdgbp():
+    '''Check that the USD/GBP exchange gives the correct value'''
+    converter = YahooFinanceConverter()
+    ticker = yfinance.Ticker("GBP=X")
+
+    # Set a date where the market is open (to avoid an empty dataframe)
+    date = datetime.datetime(2025, 3, 21)
+    
+    # Get the most recent close price
+    price = ticker.history(start=date, period="1d")["Close"].iloc[-1]
+
+    # Check the converter gives the close price for the date given
+    assert converter.get_exchange_rate("USD", "GBP", date) == price
+
+
+def test_exchange_rate_gbpusd():
+    '''Check that the GBP/USD exchange gives the correct value'''
+    converter = YahooFinanceConverter()
+    ticker = yfinance.Ticker("GBPUSD=X")
+
+    # Set a date where the market is open (to avoid an empty dataframe)
+    date = datetime.datetime(2025, 3, 21)
+    
+    # Get the most recent close price
+    price = ticker.history(start=date, period="1d")["Close"].iloc[-1]
+
+    # Check the converter gives the close price for the date given
+    assert converter.get_exchange_rate("GBP", "USD", date) == price
+
+def test_exchange_rate_date_datetime():
+    '''Check that the GBP/USD exchange gives the correct value'''
+    converter = YahooFinanceConverter()
+    ticker = yfinance.Ticker("GBPUSD=X")
+
+    # Create datetime and date objects
+    date = datetime.datetime(2025, 3, 21, 15, 24, 31, 12)
+    date2 = datetime.date(2025, 3, 21)
+
+    # Check they give the same result
+    assert converter.get_exchange_rate("GBP", "USD", date) == converter.get_exchange_rate("GBP", "USD", date2)
+
+def test_exchange_rate_return_type():
+    '''Check that the returned exchange rate is a numpy float'''
+    converter = YahooFinanceConverter()
+    assert isinstance(converter.get_exchange_rate("GBP", "RUB"), numpy.floating)
+
+def test_date_weekend_gives_week_close():
+    '''
+    Check that the rates on a Saturday is the Friday close price.
+
+    The Sunday close price would not be the same because the market reopens at
+    17:00 UTC.
+    '''
+    converter = YahooFinanceConverter()
+
+    # Create datetime and date objects
+    date = datetime.date(2026, 3, 20) # Friday
+    date2 = datetime.date(2026, 3, 21) # Saturday
+
+    # Check they give the same result
+    assert converter.get_exchange_rate("GBP", "USD", date) == converter.get_exchange_rate("GBP", "USD", date2)
+
+def test_error_unknown_currency():
+    '''Test an exception is thrown for a nonexistant currency'''
+    converter = YahooFinanceConverter()
+    with pytest.raises(Exception):
+        symbol = converter._identify_ticker("GBP", "Nonexistant Currency")
