91fb1b35efeeb8c4e9e1454bbaf0e8c446813332
Alex Stuart McNall <asm@alexmcnall.co.uk>
2026-05-18 21:40:07 +0100

    Refactor currency converter abstract base class
    Refactor to remove the need for a CurrencyConverter class and
    services based on a separate abstract base class. The
    CurrencyConverter class is now the abstract base class and the
    service implementations are subclasses of this class. As part
    of this change, instances cannot change converter service after
    object initialisation.

--- src/currency_converter/__init__.py
+++ src/currency_converter/__init__.py
@@ -1,3 +1,4 @@
 from .currency_converter import CurrencyConverter
+from .yfinance import YahooFinanceConverter
 
-__all__ = ['CurrencyConverter']
+__all__ = ['CurrencyConverter', 'YahooFinanceConverter']
